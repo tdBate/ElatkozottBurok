b70206371476f160a78e7bf9ca784c4aad2d5ea3
Török Donát <td.zazsa@gmail.com>
2026-09-15 09:23:36 +0200

    Fejleszto értékadás

--- ElatkozottBurok.Tesztek/IrodaTesztek.cs
+++ ElatkozottBurok.Tesztek/IrodaTesztek.cs
@@ -20,7 +20,7 @@ namespace ElatkozottBurok.Tesztek
             Assert.That(snack.StresszOldas, Is.EqualTo(0));
             Assert.That(snack.Ar, Is.EqualTo(100));
         }
-        /*
+       
         [Test]
         public void Fejleszto_KoffeinEsStresszHatarteruletek_EsKiegveAllapot()
         {
@@ -35,45 +35,45 @@ namespace ElatkozottBurok.Tesztek
             Assert.That(dev2.StresszSzint, Is.EqualTo(100));
             Assert.That(dev2.Kiegve, Is.True);
         }
+        /*
+[Test]
+public void Fejleszto_KedvencSnack_KetszeresStresszoldasEsExtraKoffein()
+{
+    var dev = new Fejleszto("Márk", Munkakor.Junior, 1000, "Maci Laci", 30, 50);
+    var kedvenc = new Nassolnivalo("Maci Laci", 10, 10, 200);
 
-        [Test]
-        public void Fejleszto_KedvencSnack_KetszeresStresszoldasEsExtraKoffein()
-        {
-            var dev = new Fejleszto("Márk", Munkakor.Junior, 1000, "Maci Laci", 30, 50);
-            var kedvenc = new Nassolnivalo("Maci Laci", 10, 10, 200);
+    dev.Fogyaszt(kedvenc);
 
-            dev.Fogyaszt(kedvenc);
+    // Koffein: 30 + 10 + 5 (bónusz) = 45
+    // Stressz: 50 - (10 * 2) = 30
+    Assert.That(dev.Koffeinszint, Is.EqualTo(45));
+    Assert.That(dev.StresszSzint, Is.EqualTo(30));
+}
 
-            // Koffein: 30 + 10 + 5 (bónusz) = 45
-            // Stressz: 50 - (10 * 2) = 30
-            Assert.That(dev.Koffeinszint, Is.EqualTo(45));
-            Assert.That(dev.StresszSzint, Is.EqualTo(30));
-        }
+[Test]
+public void Automata_ElakadvaAllapotban_MegnoveliAVasarloStresszet()
+{
+    var automata = new Automata { Elakadva = true };
+    var dev = new Fejleszto("Péter", Munkakor.Senior, 1000, "Kávé", 50, 20);
 
-        [Test]
-        public void Automata_ElakadvaAllapotban_MegnoveliAVasarloStresszet()
-        {
-            var automata = new Automata { Elakadva = true };
-            var dev = new Fejleszto("Péter", Munkakor.Senior, 1000, "Kávé", 50, 20);
+    var kapott = automata.Vasarlas("Kávé", dev);
 
-            var kapott = automata.Vasarlas("Kávé", dev);
+    Assert.That(kapott, Is.Null);
+    Assert.That(dev.StresszSzint, Is.EqualTo(35)); // 20 + 15
+}
 
-            Assert.That(kapott, Is.Null);
-            Assert.That(dev.StresszSzint, Is.EqualTo(35)); // 20 + 15
-        }
+[Test]
+public void Automata_KevesPenz_NemAdKiTermeket()
+{
+    var automata = new Automata();
+    automata.Feltolt(new List<Nassolnivalo> { new Nassolnivalo("Kávé", 20, 10, 300) });
+    var dev = new Fejleszto("Szegény Béla", Munkakor.Junior, 100, "Kávé");
 
-        [Test]
-        public void Automata_KevesPenz_NemAdKiTermeket()
-        {
-            var automata = new Automata();
-            automata.Feltolt(new List<Nassolnivalo> { new Nassolnivalo("Kávé", 20, 10, 300) });
-            var dev = new Fejleszto("Szegény Béla", Munkakor.Junior, 100, "Kávé");
+    var kapott = automata.Vasarlas("Kávé", dev);
 
-            var kapott = automata.Vasarlas("Kávé", dev);
-
-            Assert.That(kapott, Is.Null);
-            Assert.That(dev.Penz, Is.EqualTo(100));
-        }*/
+    Assert.That(kapott, Is.Null);
+    Assert.That(dev.Penz, Is.EqualTo(100));
+}*/
     }
 }
 
--- ElatkozottBurok/Fejleszto.cs
+++ ElatkozottBurok/Fejleszto.cs
@@ -4,6 +4,49 @@ namespace ElatkozottBurok
 {
     public class Fejleszto
     {
-    
+        private string nev;
+        private Munkakor munkakor;
+        private int penz;
+        private int koffeinSzint;
+        private int stresszSzint;
+        private bool kiegve;
+        private string kedvencSnack;
+
+        public Fejleszto(string nev, Munkakor munkakor, int penz, string kedvencSnack, int stresszSzint, int koffeinSzint)
+        {
+            this.nev = nev;
+            this.munkakor = munkakor;
+            this.penz = penz;
+            this.koffeinSzint = koffeinSzint;
+            this.stresszSzint = stresszSzint;
+            this.kiegve = false;
+            this.kedvencSnack = kedvencSnack;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public Munkakor Munkakor { get => munkakor; set => munkakor = value; }
+        public int Penz { get => penz; set  {
+                if (value < 0)
+                {
+                    penz = 0;
+                }
+                else 
+                {
+                    penz = value;
+                }
+            } }
+        public int Koffeinszint { get => koffeinSzint; set {
+                if (value < 0) { koffeinSzint = 0; }
+                else if (value >= 100) { koffeinSzint = 100; kiegve = true; }
+                else { koffeinSzint = value; }
+            }
+        }
+        public int StresszSzint { get => stresszSzint; set {
+                if (value < 0) { stresszSzint = 0; }
+                else if (value >= 100) { stresszSzint = 100; kiegve = true; }
+                else { stresszSzint = value; }
+            } }
+        public bool Kiegve { get => kiegve; set => kiegve = value; }
+        public string KedvencSnack { get => kedvencSnack; set => kedvencSnack = value; }
     }
 }
